1940931da7fb16c4efad01e022deebf3b1e93627
Alikhan <chort445@gmail.com>
2024-10-30 16:47:46 +0300

    #23 ex1

---
+++ exercise2/script1.js
@@ -0,0 +1,8 @@
+let laptopName = "Lenovo"
+let processor = "nz"
+let size; "1048"
+let color; "gray"
+let todayWeather; "cold";
+let currentTime; "16:44"
+
+console.log(laptopName, processor, size, color, todayWeather, currentTime);
